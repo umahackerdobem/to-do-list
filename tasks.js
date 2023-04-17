e2109e99ec74d5e5d9a88ad96fbb9f36d39b5acb
Myrea Narayana <umahackerdobem@gmail.com>
2023-04-17 18:56:26 -0300

    JS: Criação e manipulação de elementos

--- tasks.js
+++ tasks.js
@@ -18,23 +18,68 @@ const addTaskInput = document.getElementById("task_input");
 const addTaskButton = document.getElementsByTagName("button")[0];
 const taskList = document.getElementById("tasks_list");
 
-console.log(taskList);
+// create new task element
+function createNewTaskEl(taskName, taskId) {
+  // create task li
+  let task = document.createElement("li");
+  task.classList.add("task");
+  task.classList.add("todo");
+  task.setAttribute("id", taskId);
+  // create .left_content div
+  // todo icon
+  // done icon
+  let doneIcon = document.createElement("i");
+  doneIcon.classList.add("ph-duotone");
+  doneIcon.classList.add("ph-check-circle");
+  doneIcon.classList.add("check_btn");
+  doneIcon.classList.add("hidden");
+  doneIcon.addEventListener("click", incompleteTask);
+
+  // task name / p
+  let name = document.createElement("p");
+  name.innerHTML = taskName;
+  let leftContent = document.createElement("div");
+  leftContent.classList.add("left_content");
+
+  // todo icon
+  let todoIcon = document.createElement("i");
+  todoIcon.classList.add("ph-duotone");
+  todoIcon.classList.add("ph-circle-dashed");
+  todoIcon.classList.add("check_btn");
+  todoIcon.addEventListener("click", completeTask);
+
+  // delete icon
+  let deleteIcon = document.createElement("i");
+  deleteIcon.classList.add("ph-duotone");
+  deleteIcon.classList.add("ph-trash");
+  deleteIcon.classList.add("delete_btn");
+  deleteIcon.addEventListener("click", deleteTask);
+
+  console.log(taskList);
+  leftContent.appendChild(todoIcon);
+  leftContent.appendChild(doneIcon);
+  leftContent.appendChild(name);
+  task.appendChild(leftContent);
+  task.appendChild(deleteIcon);
+  return task;
+}
 // add new task
 function addTask(event) {
   event.preventDefault();
   console.log("Add Task");
+
+  const newTaskName = addTaskInput.value;
+
+  const newTask = {
+    id: uid(),
+    name: newTaskName,
+    toDo: true,
+  };
+
+  taskData.push(newTask);
+  const taskElement = createNewTaskEl(newTask.name, newTask.id);
+  taskList.appendChild(taskElement);
 }
-
-const newTaskName = addTaskInput.value;
-
-const newTask = {
-  id: uid(),
-  name: newTaskName,
-  toDo: true,
-};
-
-taskData.push(newTask);
-
 // complete task
 function completeTask(event) {
   console.log("Complete task");
