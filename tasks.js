53e6c1317a4c8c334951098527a91eff41600a31
Myrea Narayana <umahackerdobem@gmail.com>
2023-04-17 13:51:29 -0300

    Requisitos, funções e checkpoints

--- tasks.js
+++ tasks.js
@@ -10,3 +10,32 @@ let taskData = [
     toDo: false,
   },
 ];
+const addTaskInput = document.getElementById("task_input");
+const addTaskButton = document.getElementsByTagName("button")[0];
+const taskList = document.getElementById("tasks_list");
+
+console.log(taskList);
+// add new task
+function addTask(event) {
+  event.preventDefault();
+  console.log("Add Task");
+}
+
+// complete task
+function completeTask(event) {
+  console.log("Complete task");
+}
+
+// incomplete task
+function incompleteTask(event) {
+  console.log("Incomplete task");
+}
+
+// delete task
+function deleteTask(event) {
+  console.log("Delete Task");
+}
+
+// sync HTML with taskData list
+
+// counter tasks
